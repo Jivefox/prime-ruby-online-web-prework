369097a6973d0253e701fd79cf111c13301c1fc9
Cliff Jurgensen <jivefox.002@gmail.com>
2018-11-21 14:50:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(int)
   return false if int < 2
   return true if int == 2
-  (2..number - 1).each do |x|
-    return false if number % x == 0
+  (2..int - 1).each do |x|
+    return false if int % x == 0
   end
   true
 end    
