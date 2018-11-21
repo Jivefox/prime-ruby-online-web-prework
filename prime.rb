0857fc0988c4ca69f7a3097aba8276b17b4d7eee
Cliff Jurgensen <jivefox.002@gmail.com>
2018-11-21 14:46:53 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,4 @@
 def prime?(int)
-  if number >= 2
-    (2..number - 1).all? do |x|
-      number % x != 0
-    end
-  else
-    return false
-  end
-end
-
-def prime?(int)
   return false if int < 2
   return true if int == 2
   (2..number - 1).each do |x|
